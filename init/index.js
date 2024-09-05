fd97428de46b0bc07c8a52777fdc499e94f1e8ed
Maneeya Mehta <133096607+maneeya04@users.noreply.github.com>
2024-09-06 03:34:00 +0530

    added new features

--- init/index.js
+++ init/index.js
@@ -1,25 +1,32 @@
+if(process.env.NODE_ENV != "production") {
+    require("dotenv").config();
+}
+
 const mongoose = require("mongoose");
-const initData = require("./data.js");
+const initdata = require("./data.js");
 const Listing = require("../models/listing.js");
-const methodOverride = require("method-override");
-const MONGO_URL = "mongodb://127.0.0.1:27017/airdb";
+
+// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const DB_URL = process.env.ATLASDB_URL;
 
 main()
-  .then(() => {
+    .then(() => {
     console.log("connected to DB");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+    })
+    .catch((err) => {
+        console.log(err);
+    })
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+    // await mongoose.connect(MONGO_URL);
+    await mongoose.connect(DB_URL);
 }
 
 const initDB = async () => {
-  await Listing.deleteMany({});
-  await Listing.insertMany(initData.data);
-  console.log("data was initialized");
+    await Listing.deleteMany({});
+    initdata.data = initdata.data.map((obj) => ({...obj, owner: "653cd6bda600917744ca7927"}));
+    await Listing.insertMany(initdata.data);
+    console.log("Data was initialized");
 };
 
 initDB();
